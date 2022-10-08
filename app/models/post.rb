f139d29164a3ad1b7846196cbe26bd8c5406dd35
Muhammad Shaheer <shaheer.chry@gmail.com>
2022-10-08 19:06:46 +0500

    Linters

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,17 +1,16 @@
 class Post < ApplicationRecord
-    has_many :comments
-    has_many :likes
-    belongs_to :users
-    after_save :update_posts_counter
-  
-    def return_recent_comments
-      comments.order(created_at: :desc).limit(6)
-    end
-  
-    private
-  
-    def update_posts_counter
-      users.increment!(:posts_counter)
-    end
+  has_many :comments
+  has_many :likes
+  belongs_to :users
+  after_save :update_posts_counter
+
+  def return_recent_comments
+    comments.order(created_at: :desc).limit(6)
   end
-  
+
+  private
+
+  def update_posts_counter
+    users.increment!(:posts_counter)
+  end
+end
